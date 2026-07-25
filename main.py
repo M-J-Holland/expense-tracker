f0859b4f0778d576e1e7659ab98b962dbf691b45
Mathew <mathew_holland@hotmail.com>
2026-07-25 19:32:30 +0100

    Added a while loop for the whole program to keep it running. Added a show expenses feature.

--- main.py
+++ main.py
@@ -6,37 +6,43 @@ options = ["Add Expense", "View Expense", "Show Total", "Exit"]
 
 for index, choice in enumerate(options, start=1):
     print(f"{index} - {choice}")
-
 while True:
-    user_choice = input("\nChoice: ")
-
-    try:
-        user_choice = int(user_choice)
-        break
-    except ValueError:
-        print("Please only enter a number")
-
-if user_choice == 1:
-    with open("expenses.json", "r") as file:
-        expenses = json.load(file)
-
-    description = input("Description: ")
     while True:
+        user_choice = input("\nChoice: ")
+
         try:
-            amount = float(input("Amount: "))
+            user_choice = int(user_choice)
             break
         except ValueError:
-            print("Please only enter a number e.g 4, 3.14")
+            print("Please only enter a number")
 
-    category = input("Category: ")
+    if user_choice == 1:
+        with open("expenses.json", "r") as file:
+            expenses = json.load(file)
 
-    expense = {
-        "description": description,
-        "amount": amount,
-        "category": category
-    }
+        description = input("Description: ")
+        while True:
+            try:
+                amount = float(input("Amount: "))
+                break
+            except ValueError:
+                print("Please only enter a number e.g 4, 3.14")
 
-    expenses.append(expense)
+        category = input("Category: ")
 
-    with open("expenses.json", "w") as file:
-        json.dump(expenses, file, indent=4)
+        expense = {
+            "description": description,
+            "amount": amount,
+            "category": category
+        }
+
+        expenses.append(expense)
+
+        with open("expenses.json", "w") as file:
+            json.dump(expenses, file, indent=4)
+
+    elif user_choice == 2:
+        with open("expenses.json", "r") as file:
+            expenses = json.load(file)
+        for  expense in expenses:
+            print(f"{expense["description"]}, {expense["amount"]}, {expense["category"]}")
