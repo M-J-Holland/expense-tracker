8dbefaea2302d7e0434679185b33fb5880cb2a64
Mathew <mathew_holland@hotmail.com>
2026-07-25 16:55:34 +0100

    Added a JSON file to store the expenses. Added the add expense feature.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 print("Welcome to the CLI expense tracker.")
 
 options = ["Add Expense", "View Expense", "Show Total", "Exit"]
@@ -5,4 +7,30 @@ options = ["Add Expense", "View Expense", "Show Total", "Exit"]
 for index, choice in enumerate(options, start=1):
     print(f"{index} - {choice}")
 
-user_choice = int(input("\nChoice: "))
+while True:
+    user_choice = input("\nChoice: ")
+
+    try:
+        user_choice = int(user_choice)
+        break
+    except ValueError:
+        print("Please only enter a number")
+
+if user_choice == 1:
+    with open("expenses.json", "r") as file:
+        expenses = json.load(file)
+
+    description = input("Description: ")
+    amount = input("Amount: ")
+    category = input("Category: ")
+
+    expense = {
+        "description": description,
+        "amount": amount,
+        "category": category
+    }
+
+    expenses.append(expense)
+
+    with open("expenses.json", "w") as file:
+        json.dump(expenses, file, indent=4)
