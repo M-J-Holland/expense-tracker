33fc810623912e996436bb5f5b2d05d658a04d3d
Mathew <mathew_holland@hotmail.com>
2026-07-27 20:48:37 +0100

    Created a main function to run the code from

--- main.py
+++ main.py
@@ -1,72 +1,40 @@
-import json
+from os import name
 
-print("Welcome to the CLI expense tracker.")
+from expense_functions import add_expenses, show_expenses, show_expense_total
 
-options = ["Add Expense", "View Expenses", "Show Total", "Exit"]
 
-for index, choice in enumerate(options, start=1):
-    print(f"{index} - {choice}")
 # main while loop
-while True:
-    # user_choice while loop
+def main():
+    print("Welcome to the CLI expense tracker.")
+
+    options = ["Add Expense", "View Expenses", "Show Total", "Exit"]
+
+    for index, choice in enumerate(options, start=1):
+        print(f"{index} - {choice}")
     while True:
-        user_choice = input("\nChoice: ")
-
-        try:
-            user_choice = int(user_choice)
-            break
-        except ValueError:
-            print("Please only enter a number")
-
-    if user_choice == 1: # Add Expense
-        with open("expenses.json", "r") as file:
-            expenses = json.load(file)
-
-        description = input("Description: ")
+        # user_choice while loop
         while True:
+            user_choice = input("\nChoice: ")
             try:
-                amount = float(input("Amount: "))
+                user_choice = int(user_choice)
                 break
             except ValueError:
-                print("Please only enter a number e.g 4, 3.14")
+                print("Please only enter a number")
+        if user_choice == 1:
+            add_expenses()
+        elif user_choice == 2:
+            shown_expenses = show_expenses()
+            print(shown_expenses)
+        elif user_choice == 3:
+            expense_total = show_expense_total()
+            print(expense_total)
+        elif user_choice == 4:
+            print("Goodbye.")
+            break
+        else:
+            print("You appear to have inputted an option that doesn't seem to exist, please try again\n")
+            for index, choice in enumerate(options, start=1):
+                print(f"{index} - {choice}")
 
-        category = input("Category: ")
-
-        expense = {
-            "description": description,
-            "amount": amount,
-            "category": category
-        }
-
-        expenses.append(expense)
-
-        with open("expenses.json", "w") as file:
-            json.dump(expenses, file, indent=4)
-
-    elif user_choice == 2: # Show Expenses
-        with open("expenses.json", "r") as file:
-            expenses = json.load(file)
-        if not expenses:
-            print("You currently have no expenses to show. After you've added some, come back here to see them.")
-            continue
-        for expense in expenses:
-            print(f"{expense['description']}, {expense['amount']:.2f}, {expense['category']}")
-
-    elif user_choice == 3: # Show Expense total
-        with open("expenses.json", "r") as file:
-            expenses = json.load(file)
-        if not expenses:
-            print("You currently have no expenses to show the total. After you've added some, come back here to see "
-                  "them.")
-            continue
-        total = 0
-        for expense in expenses:
-            total += expense["amount"]
-        print(f"Your total expenses are: £{total:.2f}")
-    elif user_choice == 4:
-        print("Goodbye.")
-        break
-    else:
-        print("You appear to have inputted an option that doesn't seem to exist, please try again\n")
-        for index, choice in enumerate(options, start=1):
-            print(f"{index} - {choice}")
+if __name__ == '__main__':
+    main()
